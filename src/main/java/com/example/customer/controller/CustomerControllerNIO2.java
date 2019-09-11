77a690ac2bd2fdf19a283f42b0f24ad28cc3d47a
Wanderlei Souza <wandi.darko@gmail.com>
2019-09-11 00:06:42 -0300

    small naming fixes into json file load implementation

--- src/main/java/com/example/customer/controller/CustomerControllerNIO2.java
+++ src/main/java/com/example/customer/controller/CustomerControllerNIO2.java
@@ -5,26 +5,17 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.example.customer.repository.CustomerRepository;
 
 @RestController
 public class CustomerControllerNIO2 {
 
-	Logger logger = LoggerFactory.getLogger(CustomerControllerNIO2.class);
-
-	@Autowired
-	CustomerRepository customerService;
-    
-    @GetMapping(value = "/jsontest", produces = MediaType.APPLICATION_JSON_VALUE)
-    public String findJsonFile() throws IOException {
-    	Path path = Paths.get("src/main/resources/sample.json"); 
+    @GetMapping(value = "/files/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public String getPathById(@PathVariable final String id) throws IOException {
+    	Path path = Paths.get("src/main/resources/customer_".concat(id).concat(".json")); 
     	return new String(Files.readAllBytes(path));
     }
 
