2c7b170991a557c730502ff9674338b2e9a3e1d8
Wanderlei Souza <wandi.darko@gmail.com>
2019-09-17 18:02:20 -0300

    nio example controller

--- src/main/java/com/example/customer/controller/CustomerControllerNIO2.java
+++ src/main/java/com/example/customer/controller/CustomerControllerNIO2.java
@@ -1,9 +1,11 @@
 package com.example.customer.controller;
 
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.zip.ZipInputStream;
 
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,9 +16,28 @@ import org.springframework.web.bind.annotation.RestController;
 public class CustomerControllerNIO2 {
 
     @GetMapping(value = "/files/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
-    public String getPathById(@PathVariable final String id) throws IOException {
+    public String getFileById(@PathVariable final String id) throws IOException {
     	Path path = Paths.get("src/main/resources/customer_".concat(id).concat(".json")); 
     	return new String(Files.readAllBytes(path));
+    }
+    
+    @GetMapping(value = "/files/unzip/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public String unzipFileById(@PathVariable final String id) throws IOException {
+    	
+    	Path path = Paths.get("src/main/resources/customer_".concat(id).concat(".json.zip")); 
+		ZipInputStream zis = new ZipInputStream(new FileInputStream(path.toAbsolutePath().toString()));
+		StringBuilder s = new StringBuilder();
+		byte[] buffer = new byte[1024];
+		int read = 0;
+		while ((zis.getNextEntry()) != null) {
+			while ((read = zis.read(buffer, 0, 1024)) >= 0) {
+				s.append(new String(buffer, 0, read));
+			}
+		}
+		zis.close();
+		
+		return s.toString();
+		
     }
 
 }
