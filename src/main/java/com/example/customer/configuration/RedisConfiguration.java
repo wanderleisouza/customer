03541aebc54f4ed534f4c6a6657b7551333da5cd
Wanderlei Souza <wandi.darko@gmail.com>
2019-09-19 00:21:25 -0300

    fix redis connection

--- src/main/java/com/example/customer/configuration/RedisConfiguration.java
+++ src/main/java/com/example/customer/configuration/RedisConfiguration.java
@@ -1,15 +1,36 @@
 package com.example.customer.configuration;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.jedis.JedisClientConfiguration;
+import org.springframework.data.redis.connection.jedis.JedisClientConfiguration.JedisClientConfigurationBuilder;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+
 
 @Configuration
 public class RedisConfiguration {
 
-	  @Bean
-	  public JedisConnectionFactory redisConnectionFactory() {
-	    return new JedisConnectionFactory();
-	  }
+    @Value("${spring.redis.host:localhost}")
+    private String redisHostName;
+
+    @Value("${spring.redis.port:6379}")
+    private int redisPort;
+
+    @Bean
+    JedisConnectionFactory jedisConnectionFactory() {
+
+        RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration();
+        redisStandaloneConfiguration.setHostName(redisHostName);
+        redisStandaloneConfiguration.setPort(redisPort);
+        
+        JedisClientConfigurationBuilder jedisClientConfiguration = JedisClientConfiguration.builder();
+        JedisConnectionFactory jedisConFactory = new JedisConnectionFactory(redisStandaloneConfiguration, jedisClientConfiguration.build());
+
+        return jedisConFactory;
+        
+    }
+    
 
 }
