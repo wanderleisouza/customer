146e7c2d423b2d73a8544eb47305722e0e724a84
Wanderlei Souza <wandi.darko@gmail.com>
2020-07-27 14:14:31 -0300

    serialversion uuid

--- src/main/java/com/example/customer/exception/CustomerNotFoundException.java
+++ src/main/java/com/example/customer/exception/CustomerNotFoundException.java
@@ -4,4 +4,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(code = HttpStatus.NOT_FOUND, reason = "Customer not found")
-public class CustomerNotFoundException extends RuntimeException { }
+public class CustomerNotFoundException extends RuntimeException {
+	private static final long serialVersionUID = -3900345695258500752L; 
+}
